f1d0ffd3a30569e5f010530e7b6e21dc803c955d
Pramod153 <pramodsa153155@gmail.com>
2025-03-27 20:39:48 +0530

    OOPS

---
+++ OOPS/access_modifier_II/src/Main.java
@@ -0,0 +1,16 @@
+package access_modifier_2;
+
+import access_modifier_1.car;
+import access_modifier_1.Santro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		car obj=new car();
+		System.out.println(obj.data);
+		Santro s1 =new Santro();
+		System.out.println(s1.data3);
+	}
+
+}
---
+++ OOPS/access_modifier_II/src/Maruti.java
@@ -0,0 +1,8 @@
+package access_modifier_2;
+
+public class Maruti extends car {
+	public void show() {
+		System.out.println(data3);
+	}
+
+}
